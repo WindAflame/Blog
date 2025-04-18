7588dbeea35dec119a9a8f2b9146ab15337d5cda
WindAflame <dev.windaflame@gmail.com>
2025-04-18 13:40:53 +0200

    chore: optimize and format

--- scripts/python/main.py
+++ scripts/python/main.py
@@ -17,6 +17,7 @@ logger = logging.getLogger(__name__)
 # Constants
 DESTINATION_DIRECTORY = "static"
 
+
 # Methods
 def __requests(
     wrapper: IGDBWrapper, url_name: str, key_name: str | None, requests_id: any
@@ -37,10 +38,12 @@ def __retrieve_game_information(wrapper: IGDBWrapper, game_id: str) -> Any:
     if (data := __read_game_json(game_id)) != None:
         logger.info("Game data finded at dest folder. Load it instead of request API.")
         return data
+    fields = "id,aggregated_rating,artworks,genres,involved_companies,name,parent_game,platforms,rating,slug,summary,videos,game_type"
     byte_array = wrapper.api_request(
-        "games", f"fields *; offset 0; where id={game_id};"
+        "games", f"fields {fields}; offset 0; where id={game_id};"
     )
     return json.loads(byte_array)[0]
+
 
 def __read_game_json(game_id: str) -> Any | None:
     filepath = os.path.join(DESTINATION_DIRECTORY, game_id + ".json")
@@ -86,29 +89,33 @@ def __transform_attribut_from_data(
         result = __requests(wrapper, url_name, key_name, ids)[0]
         return result if not callback else callback(result, wrapper)
 
+
 def __transform_to_slug_item(data: dict, wrapper: IGDBWrapper):
     return {"name": data["name"], "slug": data["slug"]}
 
+
 def __transform_to_slug_list(datas: list[dict], wrapper: IGDBWrapper):
-    return list(
-        map(lambda i: __transform_to_slug_item(i, wrapper), datas)
-    )
+    return list(map(lambda i: __transform_to_slug_item(i, wrapper), datas))
+
 
 def __transform_artworks(datas: list[dict], wrapper: IGDBWrapper):
     return list(map(lambda i: i["image_id"], datas))
 
+
 def __transform_companies(involved_companies_data: list[dict], wrapper: IGDBWrapper):
-    for (index, involved_company_data) in enumerate(involved_companies_data):
-        involved_companies_data[index] = __transform_attribut_from_data(wrapper, "companies", "company", involved_company_data, None)
+    for index, involved_company_data in enumerate(involved_companies_data):
+        involved_companies_data[index] = __transform_attribut_from_data(
+            wrapper, "companies", "company", involved_company_data, None
+        )
     return __transform_to_slug_list(involved_companies_data, wrapper)
+
 
 def __transform_game_type(data: dict, wrapper: IGDBWrapper):
     return data["type"]
 
+
 def __transform_to_video_list(datas: list[dict], wrapper: IGDBWrapper):
-    return list(
-        map(lambda i: i["video_id"], datas)
-    )
+    return list(map(lambda i: i["video_id"], datas))
 
 
 # Application
@@ -134,7 +141,7 @@ def main():
         ("involved_companies", "involved_companies", __transform_companies),
         ("games", "parent_game", __transform_to_slug_item),
         ("platforms", "platforms", __transform_to_slug_list),
-        ("game_videos", "videos", __transform_to_video_list)
+        ("game_videos", "videos", __transform_to_video_list),
     ]:
         if data_transformed := __transform_attribut_from_data(
             wrapper, url, key, game_data, callback
