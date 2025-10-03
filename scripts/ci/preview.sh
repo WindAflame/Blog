dada1b691a82358ee4ab2cfff7db40e5cc13d60d
WindAflame <dev.windaflame@gmail.com>
2025-10-03 11:49:23 +0200

    feat: add netlify ci

---
+++ scripts/ci/preview.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ACTIVE_PROJECT=sources/zola-linkita
+echo "Check if submodule has new content"
+if git diff --quiet HEAD^ HEAD -- "$ACTIVE_PROJECT"; then
+    echo "Submodule $ACTIVE_PROJECT has NOT been updated."
+else
+    echo "Submodule $ACTIVE_PROJECT has been updated."
+    git submodule update --init --remote
+    echo "Build the project in preview ..."
+    zola build --base-url $DEPLOY_PRIME_URL
+fi
