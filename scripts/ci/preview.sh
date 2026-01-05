e2eb82c26f972f94d98f5387bdca1e8487102ddb
WindAflame <dev.windaflame@gmail.com>
2026-01-05 10:02:36 +0100

    chore: simplier scripts

--- scripts/ci/preview.sh
+++ scripts/ci/preview.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 ACTIVE_PROJECT=sources/zola-linkita
-echo "Check if submodule has new content"
-if git diff --quiet HEAD^ HEAD -- "$ACTIVE_PROJECT"; then
-    echo "Submodule $ACTIVE_PROJECT has NOT been updated."
-else
-    echo "Submodule $ACTIVE_PROJECT has been updated."
-    git submodule update --init --remote
-    echo "Build the project in preview ..."
-    zola build --base-url $DEPLOY_PRIME_URL
-fi
+
+echo "Updating submodule $ACTIVE_PROJECT..."
+git submodule update --init --recursive
+
+echo "Building the project in preview mode..."
+cd $ACTIVE_PROJECT
+zola build --base-url $DEPLOY_PRIME_URL
